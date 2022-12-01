7b054a23184add873711386d1722f0d4c83b7790
king <chenxie86@gmail.com>
2022-12-02 00:24:15 +0800

    refactor: change telescope layout

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -1,9 +1,21 @@
 require("telescope").setup({
+    defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+            height = 0.95,
+            prompt_position = 'top',
+        },
+    },
+    pickers = {
+        find_files = {
+            theme = "dropdown"
+        }
+    },
     extensions = {
         coc = {
             theme = 'ivy',
             prefer_locations = true, -- always use Telescope locations to preview definitions/declarations/implementations etc
-        }
+        },
     },
 })
 require('telescope').load_extension('coc')
